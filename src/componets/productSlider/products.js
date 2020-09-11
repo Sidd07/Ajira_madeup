dfe67099a33b2db9ab5f8197c27113e753407762
Sidd07 <sdhandarfale@gmail.com>
2020-09-11 20:44:35 +0530

    images updated

--- src/componets/productSlider/products.js
+++ src/componets/productSlider/products.js
@@ -3,14 +3,17 @@ import "./product.scss";
 
 const Products =()=>{
 
-        let productData = [{ name: "Printed Polo T-Shirt", price: "1499" }, { name: "Printed Polo T-Shirt", price: "1499" }, { name: "Printed Polo T-Shirt", price: "1499" }, { name: "Printed Polo T-Shirt", price: "1499" }, { name: "Printed Polo T-Shirt", price: "1499" }]
+        let productData = [{ image: "collection1",name: "Printed Polo T-Shirt", price: "1499" }, { image: "collection2",name: "Printed Polo T-Shirt", price: "1499" }, {image: "collection1", name: "Printed Polo T-Shirt", price: "1499" }, { image: "collection2",name: "Printed Polo T-Shirt", price: "1499" }, { image: "collection1", name: "Printed Polo T-Shirt", price: "1499" }]
         return (
             <div className="slider">
            { productData.map((listValue, index) => {
                 return (                   
                         <div className="block-20" key={index}>
+                           {listValue.image==="collection1"?
                             <div className="product-image m-20">
-                            </div>
+                            </div>:
+                            <div className="product-image-1 m-20">
+                            </div>}
                             <label>{listValue.name}</label>
                             <br/>
                             <p className="text-orange">{listValue.price}</p>
--- src/container/landing.js
+++ src/container/landing.js
@@ -7,11 +7,10 @@ class LandingPage extends Component {
     render() {
         return (
             <div>
-                <div className="site-blocks-cover" >
+                <div className="site-blocks-cover">
                     <div className="text-block">
                         <div>
                             <p className="display-1">Clothes that <span className="text-orange">respire</span></p>
-
                             <p className="display-1"> for man that <span className="text-orange">aspire</span></p>
                             <br />
                             <button className="btn btn-orange mt-20"> Browse Collection </button>
@@ -20,8 +19,8 @@ class LandingPage extends Component {
                 </div>
 
                 <div className="row mt-10 mb-10">
-                    <div className="block-50 mt-10">
-                        <div className="site-blocks-cover" >
+                    <div className="block-50 mt-10 pr-10 sm-p-0">
+                        <div className="site-block-1" >
                             <div className="text-block">
                                 <div>
                                     <p className="display-1"> <span className="text-orange">Summer</span> is here and</p>
@@ -31,14 +30,14 @@ class LandingPage extends Component {
                         </div>
                     </div>
                     <div className="block-50 mt-10">
-                        <div className="image-gallery" >
+                        <div className="image-gallery-1" >
                             <div className="text-block">
                                 <div>
                                     <p className="display-1"><span className="text-orange">wrinkle-free </span>t-shirts</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="image-gallery  mt-10" >
+                        <div className="image-gallery-2 mt-10" >
                             <div className="text-block">
                                 <div>
                                     <p className="display-1"><span className="text-orange">Stain-free </span>Denim shirts</p>
